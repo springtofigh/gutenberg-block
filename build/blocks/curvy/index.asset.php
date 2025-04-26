499f6c0280fc5aa785f42d0370930dd7ad56dc6f
BT <bahartofigh@gmail.com>
2025-04-26 19:25:39 +0330

    block variations and block preview

--- build/blocks/curvy/index.asset.php
+++ build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b3e444145a1dda45c68c');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b42749a1ca25ca63f697');
