37c649e8752d8bd492244b5fb284a5c84a9440ff
BT <bahartofigh@gmail.com>
2025-05-03 21:30:17 +0330

    branch changes

--- build/blocks/curvy/index.asset.php
+++ build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b42749a1ca25ca63f697');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c604aba05968603b14d2');
